aea84c8bbdd4d518c97288591c1b5ad8607ec761
Diego Moya <dimoyahi@outlook.es>
2020-12-13 20:56:36 -0500

    20201213
    Se crearon los escenarios

--- AhorcaCuento/src/application/Main.java
+++ AhorcaCuento/src/application/Main.java
@@ -18,7 +18,6 @@ public class Main extends Application {
 		        Scene scene = new Scene(inicio);
 		        primaryStage.setScene(scene);
 		        primaryStage.show();
-			
 
 		} catch (Exception e) {
 			e.printStackTrace();
