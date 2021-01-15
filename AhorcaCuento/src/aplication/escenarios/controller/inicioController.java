8d1777761210bf1fee89ddcaae510fb16bb8be4f
dimoyahi <dimoyahi@users.noreply.github.com>
2021-01-15 15:19:59 -0500

    20210115

--- AhorcaCuento/src/aplication/escenarios/controller/inicioController.java
+++ AhorcaCuento/src/aplication/escenarios/controller/inicioController.java
@@ -20,7 +20,7 @@ public class inicioController {
 			"/imagenes/ahorcado/ahorcado_5.jpg", "/imagenes/ahorcado/ahorcado_6.jpg" };
 
 	int indice = 0;
-	
+
 	@FXML
 	private Pane cuento;
 	@FXML
@@ -39,61 +39,52 @@ public class inicioController {
 	private Pane inicio;
 	@FXML
 	private ImageView fondo;
-    @FXML
-    private Label textoAhorcado;
-    @FXML
-    private Button A;
-	
+	@FXML
+	private Label textoAhorcado;
+	@FXML
+	private Button A;
+
 	final int INTENTOS_TOTALES = 10;
 	int intentos = 0;
 	int aciertos = 0;
 	char resp;
-	
+	char letra;
+	double i = 0.0;
 
+	// Event Listener on Button.onAction
+	@FXML
+	public void iniciarJuaego(ActionEvent event) {
+		Juego.setVisible(true);
+		limpiar();
+		nuevoJuego(event);
+	}
+
+	@FXML
+	public void Salir(ActionEvent event) {
+		inicio.setVisible(true);
+	}
+
+	@FXML
+	public void Nprincipal(ActionEvent event) {
+		inicio.setVisible(true);
+	}
+
+	public void limpiar() {
+		textoAhorcado.setText(" ");
+		ahorcado.setImage(new Image("/imagenes/ahorcado/ahorcado_0.jpg"));
+	}
 	
-	public void logJuego() {
-		Scanner teclado = new Scanner(System.in);
-		teclado.useDelimiter("\n");
-		// Se declara la variable que genera las palablas de forma aleatoria
-		Random rnd = new Random();
-		// Se crea un array de las palaras que se van a utiizar en el Juego
-		String arrayPalabras[] = new String[7];
-		// Palabras del Juego
-		arrayPalabras[0] = "michin";
-		arrayPalabras[1] = "encontro";
-		arrayPalabras[2] = "robar";
-		arrayPalabras[3] = "gente";
-		arrayPalabras[4] = "morira";
-		arrayPalabras[5] = "impedirlo";
-		arrayPalabras[6] = "monte";
-	
+	public void logCuento(char[] tusRespuestas,char[] descomponer,char[] copia) {
 		do {
-
-			// Descomponemos las palabras que se van a buscar
-			int alea = rnd.nextInt(7);
-			char[] descomponer = descompone(arrayPalabras[alea]);
-			char[] copia = descompone(arrayPalabras[alea]);
-
-			// Array que muestras caracteres en pantalla
-			char[] tusRespuestas = new char[descomponer.length];
-
-			// Rellenamos palabras con guiones
-			for (int i = 0; i < tusRespuestas.length; i++) {
-				tusRespuestas[i] = '_';
-			}
-
-			// Empezamos a pintar las palabras
-			System.out.println("Las palabras del Cuento!");
-			frase.setText("hola");;
 			// Mientras que no nos pasemos con los intentos y no la acertemos...
 			while (intentos < INTENTOS_TOTALES && aciertos != tusRespuestas.length) {
 				imprimeOculta(tusRespuestas);
 				// Preguntamos la letra por teclado
 				//System.out.println("\nIngrese una letra: ");
-//				resp = teclado.next().toLowerCase().charAt(0);
+				resp = letra;
 				// Recorremos el array y comprobamos si se ha producido un acierto
 				for (int i = 0; i < descomponer.length; i++) {
-					if (descomponer[i] == 0) {
+					if (descomponer[i] == resp) {
 						tusRespuestas[i] = descomponer[i];
 						descomponer[i] = ' ';
 						aciertos++;
@@ -117,224 +108,192 @@ public class inicioController {
 			intentos = 0;
 			aciertos = 0;
 			// Volvemos a preguntarle al usuario si quiere volver a jugar
-			resp = pregunta("\n\nQuieres volver a jugar?", teclado);
+//			resp = pregunta("\n\nQuieres volver a jugar?", letra);
 		} while (resp != 'n');
-
 	}
 	
-	double i = 0.0;
-
-
-	// Event Listener on Button.onAction
-	@FXML
-	public void iniciarJuaego(ActionEvent event) {
-		Juego.setVisible(true);
-		
-
-	}
-
-	// Event Listener on Button.onAction
-	@FXML
-	public void Salir(ActionEvent event) {
-		inicio.setVisible(true);
-	}
-
-	// Event Listener on Button.onAction
-	@FXML
-	public void Nprincipal(ActionEvent event) {
-		inicio.setVisible(true);
-		cuento();
-		Juego.setVisible(false);
-	}
-
-	// Event Listener on Button.onAction
+	
 	@FXML
 	public void nuevoJuego(ActionEvent event) {
-		logJuego();
+		cuento();
+		limpiar();
+//		logJuego();
+		Random rnd = new Random();
+		// Se crea un array de las palaras que se van a utiizar en el Juego
+		String arrayPalabras[] = new String[7];
+		// Palabras del Juego
+		arrayPalabras[0] = "michin";
+		arrayPalabras[1] = "encontro";
+		arrayPalabras[2] = "robar";
+		arrayPalabras[3] = "gente";
+		arrayPalabras[4] = "morira";
+		arrayPalabras[5] = "impedirlo";
+		arrayPalabras[6] = "monte";
+		
+		int alea = rnd.nextInt(7);
+		char[] descomponer = descompone(arrayPalabras[alea]);
+		char[] copia = descompone(arrayPalabras[alea]);
+
+		// Array que muestras caracteres en pantalla
+		char[] tusRespuestas = new char[descomponer.length];
+
+		// Rellenamos palabras con guiones
+		for (int i = 0; i < tusRespuestas.length; i++) {
+			tusRespuestas[i] = '_';
+			String valActual = textoAhorcado.getText();
+			textoAhorcado.setText(valActual + tusRespuestas[i] + " " );
+		}
 	}
 
-	// Event Listener on Button.onAction
 	@FXML
 	public void bto_A(ActionEvent event) {
-		String url = imgAhorcado[0];
-		ahorcado.setImage(new Image(url));
+		letra = 'A';
 	}
 
-	// Event Listener on Button.onAction
 	@FXML
 	public void bto_B(ActionEvent event) {
-		String url = imgAhorcado[1];
-		ahorcado.setImage(new Image(url));
+		letra = 'B';
 	}
 
-	// Event Listener on Button.onAction
 	@FXML
 	public void bto_C(ActionEvent event) {
-		String url = imgAhorcado[2];
-		ahorcado.setImage(new Image(url));
+		letra = 'C';
 	}
 
-	// Event Listener on Button.onAction
 	@FXML
 	public void bto_D(ActionEvent event) {
-		String url = imgAhorcado[3];
-		ahorcado.setImage(new Image(url));
+		letra = 'D';
 	}
 
-	// Event Listener on Button.onAction
 	@FXML
 	public void bto_E(ActionEvent event) {
-		String url = imgAhorcado[4];
-		ahorcado.setImage(new Image(url));
+		letra = 'E';
 	}
 
-	// Event Listener on Button.onAction
 	@FXML
 	public void bto_F(ActionEvent event) {
-		String url = imgAhorcado[5];
-		ahorcado.setImage(new Image(url));
+		letra = 'F';
 	}
 
-	// Event Listener on Button.onAction
 	@FXML
 	public void bto_G(ActionEvent event) {
-		// TODO Autogenerated
+		letra = 'G';
 		incorrecto();
 	}
 
-	// Event Listener on Button.onAction
 	@FXML
 	public void bto_H(ActionEvent event) {
-		// TODO Autogenerated
+		letra = 'H';
 	}
 
-	// Event Listener on Button.onAction
 	@FXML
 	public void bto_I(ActionEvent event) {
-		// TODO Autogenerated
+		letra = 'I';
 	}
 
-	// Event Listener on Button.onAction
 	@FXML
 	public void bto_J(ActionEvent event) {
-		// TODO Autogenerated
+		letra = 'J';
 	}
 
-	// Event Listener on Button.onAction
 	@FXML
 	public void bto_K(ActionEvent event) {
-		// TODO Autogenerated
+		letra = 'K';
 	}
 
-	// Event Listener on Button.onAction
 	@FXML
 	public void bto_L(ActionEvent event) {
-		// TODO Autogenerated
+		letra = 'L';
 	}
 
-	// Event Listener on Button.onAction
 	@FXML
 	public void bto_M(ActionEvent event) {
-		// TODO Autogenerated
+		letra = 'M';
 	}
 
-	// Event Listener on Button.onAction
 	@FXML
 	public void bto_N(ActionEvent event) {
-		// TODO Autogenerated
+		letra = 'N';
 	}
 
-	// Event Listener on Button.onAction
 	@FXML
 	public void bto_NN(ActionEvent event) {
-		// TODO Autogenerated
+		letra = 'Ñ';
 	}
 
-	// Event Listener on Button.onAction
 	@FXML
 	public void bto_O(ActionEvent event) {
-		// TODO Autogenerated
+		letra = 'O';
 	}
 
-	// Event Listener on Button.onAction
 	@FXML
 	public void bto_P(ActionEvent event) {
-		// TODO Autogenerated
+		letra = 'P';
 	}
 
-	// Event Listener on Button.onAction
 	@FXML
 	public void bto_Q(ActionEvent event) {
-		// TODO Autogenerated
+		letra = 'Q';
 	}
 
-	// Event Listener on Button.onAction
 	@FXML
 	public void bto_R(ActionEvent event) {
-		// TODO Autogenerated
+		letra = 'R';
 	}
 
-	// Event Listener on Button.onAction
 	@FXML
 	public void bto_S(ActionEvent event) {
-		// TODO Autogenerated
+		letra = 'S';
 	}
 
 	// Event Listener on Button.onAction
 	@FXML
 	public void bto_T(ActionEvent event) {
-		// TODO Autogenerated
+		letra = 'T';
 	}
 
 	// Event Listener on Button.onAction
 	@FXML
 	public void bto_U(ActionEvent event) {
-		// TODO Autogenerated
+		letra = 'U';
 	}
 
 	// Event Listener on Button.onAction
 	@FXML
 	public void bto_V(ActionEvent event) {
-		// TODO Autogenerated
+		letra = 'V';
 	}
 
-	// Event Listener on Button.onAction
 	@FXML
 	public void bto_W(ActionEvent event) {
-		// TODO Autogenerated
+		letra = 'W';
 	}
 
-	// Event Listener on Button.onAction
 	@FXML
 	public void bto_X(ActionEvent event) {
-		// TODO Autogenerated
+		letra = 'X';
 	}
 
-	// Event Listener on Button.onAction
 	@FXML
 	public void bto_Y(ActionEvent event) {
-		// TODO Autogenerated
+		letra = 'Y';
 	}
 
-	// Event Listener on Button.onAction
 	@FXML
 	public void bto_Z(ActionEvent event) {
-		// TODO Autogenerated
+		letra = 'Z';
 	}
 
-	// Event Listener on Button.onAction
 	@FXML
 	public void clic_Salir(ActionEvent event) {
 		System.exit(0);
 	}
 
-	// Event Listener on Button.onAction
 	@FXML
 	public void clic_Tutorial(ActionEvent event) {
-		// TODO Autogenerated
+		bto_A(event);
 	}
 
-	// Event Listener on Button.onAction
 	@FXML
 	public void clic_Config(ActionEvent event) {
 		// TODO Autogenerated
@@ -349,8 +308,9 @@ public class inicioController {
 	// Event Listener on Button.onAction
 	@FXML
 	public void clic_IniciarJuego(ActionEvent event) {
-		cuento.setVisible(true);
 		inicio.setVisible(false);
+		Juego.setVisible(false);
+		cuento.setVisible(true);
 		cuento();
 	}
 
@@ -367,13 +327,13 @@ public class inicioController {
 
 	public void cuento() {
 		String cuentos;
-		cuentos = "Michín dijo a su mamá: Voy a volverme Pateta, "
-				+ "y el que a impedirlo se meta en el acto morirá. " + "Ya le he robado a papá  "
-				+ "daga y pistolas; ya estoy armado y listo; y me voy  " + "a robar y matar gente,  "
-				+ "y nunca más (¡ten presente!) verás a Michín desde hoy  "
+		cuentos = "Michín dijo a su mamá: Voy a volverme Pateta, " + "y el que a impedirlo se meta en el acto morirá. "
+				+ "Ya le he robado a papá  " + "daga y pistolas; ya estoy armado y listo; y me voy  "
+				+ "a robar y matar gente,  " + "y nunca más (¡ten presente!) verás a Michín desde hoy  "
 				+ "Yéndose al monte, encontró a un gallo por el camino,  " + "y dijo: A ver qué tal tino  "
 				+ "para matar tengo yo \n";
 		cuentoCompleto.setText(cuentos);
+		frase.setText(cuentos);
 	}
 
 	public void progress() {
@@ -386,15 +346,17 @@ public class inicioController {
 //			i=i+0.01;
 //		}
 	}
+
 	public void imprimeOculta(char[] tusRespuestas) {
+		
+		
 		for (int i = 0; i < tusRespuestas.length; i++) {
-			System.out.print(tusRespuestas[i] + " ");
-//			textoAhorcado.setText(tusRespuestas[i] + " ");
-			char respuesta;
-			respuesta = tusRespuestas[i];
-			textoAhorcado.setText("_______________");
+//			System.out.print(tusRespuestas[i] + " ");
+			String valActual = textoAhorcado.getText();
+			textoAhorcado.setText(valActual + tusRespuestas[i] + " " );
 		}
 	}
+
 	public static char pregunta(String men, Scanner teclado) {
 		char resp;
 		System.out.println(men + " (s/n)");
@@ -405,6 +367,7 @@ public class inicioController {
 		}
 		return resp;
 	}
+
 	private static char[] descompone(String palAzar) {
 		char[] letras;
 		letras = new char[palAzar.length()];
@@ -413,14 +376,15 @@ public class inicioController {
 		}
 		return letras;
 	}
+
 	public void PresionBto() {
 		A.setText("A");
 	}
-	
+
 	public void incorrecto() {
 		if (indice == 6) {
 			indice = 6;
-		}else {
+		} else {
 			indice++;
 			String url = imgAhorcado[indice];
 			ahorcado.setImage(new Image(url));
